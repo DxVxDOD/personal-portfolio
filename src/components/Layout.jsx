82ffbc7410602f5c3126e364bf5244f5246e0272
davidOrbang <orbangdavid@yahoo.com>
2023-02-26 13:57:11 +0000

    mesh_animation start point

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,6 @@
-import React, { Suspense } from 'react';
 import '../App.css';
 import NameCard from './NameCard';
 import NavCard from './NavCard';
-import AnimationCanvas from './AnimationCanvas';
 
 function Layout() {
   return (
@@ -10,9 +8,6 @@ function Layout() {
     dark:border-zinc-200 border flex justify-center items-center shadow-layoutBoxShadow "
     >
       <div className=" relative w-full h-full">
-        <Suspense fallback={<div>Loading....</div>} >
-          <AnimationCanvas />
-        </Suspense>
       </div>
       <NameCard />
       <NavCard />
