c8dced99995e9070cb7d01cb8820aa21cab42ddc
davidOrbang <orbangdavid@yahoo.com>
2023-02-27 17:17:38 +0000

    points animation finished

--- src/components/Points.jsx
+++ src/components/Points.jsx
@@ -1,74 +1,77 @@
-import { useFrame, useLoader } from '@react-three/fiber'
-import React, { useCallback, useMemo, useRef } from 'react'
-import circlePNG from '../assets/Untitled (2).png'
-import * as THREE from 'three'
+/* eslint-disable react/no-unknown-property */
+import { useFrame, useLoader } from '@react-three/fiber';
+import React, { useCallback, useMemo, useRef } from 'react';
+import * as THREE from 'three';
+import circleSVG from '../assets/Untitled (3).svg';
 
-const Points = () => {
-    const pngTexture = useLoader(THREE.TextureLoader, circlePNG)
-    const bufferRef = useRef()
+function Points() {
+  const svgTexture = useLoader(THREE.TextureLoader, circleSVG);
+  const bufferRef = useRef();
 
-    let t = 0;
-    let f = 0.002;
-    let a = 3;
-    const graph = useCallback((x, z) => {
-        return Math.sin(f * (x + (z ** 2) + t)) * a;
-    }, [t, a, f])
+  let t = 0;
+  const f = 0.005;
+  const a = 4;
+  const graph = useCallback((x, z) => Math.sin(
+    f * (x ** 2 + (z * 8) * 8 + t),
+  ) * a, [t, a, f]);
 
-    const count = 300
-    const sep = 0.5
-    const positions = useMemo(() => {
-        let positions = [];
-        
-        for (let xi = 0; xi < count; xi++){
-            for (let zi = 0; zi < count; zi++){
-                let x = sep * (xi - count / 2);
-                let z = sep * (zi - count / 2);
-                let y = graph(x, z);
-                positions.push(x, y, z)
-            }
-        }
+  const count = 250;
+  const sep = 0.25;
+  const positions = useMemo(() => {
+    const positionsArray = [];
 
-        return new Float32Array(positions)
-    }, [count, sep, graph])
+    for (let xi = 0; xi < count; xi += 1) {
+      for (let zi = 0; zi < count; zi += 1) {
+        const x = sep * (xi - count / 2);
+        const z = sep * (zi - count / 2);
+        const y = graph(x, z);
+        positionsArray.push(x, y, z);
+      }
+    }
 
-    useFrame(() => {
-        t += 10;
-        const positions = bufferRef.current.array
-        let i = 0
+    return new Float32Array(positionsArray);
+  }, [count, sep, graph]);
 
-        for (let xi = 0; xi < count; xi++){
-            for (let zi = 0; zi < count; zi++){
-                let x = sep * (xi - count / 2);
-                let z = sep * (zi - count / 2);
-                positions[i + 1] = graph(x, z);
-                i += 3
-            }
-        }
-        bufferRef.current.needsUpdate = false;
-    })
+  useFrame(() => {
+    t += 0.5;
+    const positionsBufferRef = bufferRef.current.array;
+    let i = 0;
+
+    for (let xi = 0; xi < count; xi += 1) {
+      for (let zi = 0; zi < count; zi += 1) {
+        const x = sep * (xi - count / 2);
+        const z = sep * (zi - count / 2);
+        positionsBufferRef[i + 1] = graph(x, z);
+        i += 3;
+      }
+    }
+    bufferRef.current.needsUpdate = true;
+  });
 
   return (
     <points>
-        <bufferGeometry 
-        attach="geometry">
-            <bufferAttribute
-            ref={bufferRef}
-            attach='attributes-position'
-            array={positions}
-            count={positions.length / 3}
-            itemSize={3}
-             />
-        </bufferGeometry>
-        <pointsMaterial 
+      <bufferGeometry
+        attach="geometry"
+      >
+        <bufferAttribute
+          ref={bufferRef}
+          attach="attributes-position"
+          array={positions}
+          count={positions.length / 3}
+          itemSize={3}
+        />
+      </bufferGeometry>
+      <pointsMaterial
         attach="material"
-        map={pngTexture}
-        size={0.5}
+        map={svgTexture}
+        size={0.2}
         sizeAttenuation
         transparent={false}
-        alphaTest={0.5}
-        opacity={1.0} />
+        alphaTest={0.2}
+        opacity={1.0}
+      />
     </points>
-  )
+  );
 }
 
-export default Points
+export default Points;
